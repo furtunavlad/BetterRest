c50dddba2f81d1f4237ea238cc7d495577ac4478
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-02-26 20:23:17 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vlad Furtuna on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
